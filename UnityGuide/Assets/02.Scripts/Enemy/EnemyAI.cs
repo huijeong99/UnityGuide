c76f10be00e372edbfc421a8e801f908a8abe07f
나희정 <hwayuya@gmail.com>
2020-06-05 17:42:21 +0900

    적 AI 네비게이터 구현(애니메이션 미구현)

---
+++ UnityGuide/Assets/02.Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public enum State
+    {
+        PATROL,
+        TRACE,
+        ATTACK,
+        DIE
+    }
+
+    public State state = State.PATROL;
+
+    private Transform playerTr;
+    private Transform enemyTr;
+    private Animator animator;
+
+    //공격 사정거리
+    public float attackDist = 5.0f;
+    //추적 사정거리
+    public float traceDist = 10.0f;
+
+    public bool isDie = false;
+
+    private WaitForSeconds ws;
+    private MoveAgent moveAgent;
+
+    private readonly int hashMove = Animator.StringToHash("IsMove");
+    private readonly int hashSpeed = Animator.StringToHash("Speed");
+
+    //여기까지 수정함
+    private void Awake()
+    {
+        var player = GameObject.FindGameObjectWithTag("PLAYER");
+        if (player != null)
+        
+        playerTr = player.GetComponent<Transform>();
+        enemyTr = GetComponent<Transform>();
+        moveAgent = GetComponent<MoveAgent>();
+        ws=new WaitForSeconds(0.3f);
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(CheckState());
+        StartCoroutine(Action());
+    }
+    
+
+    IEnumerator CheckState()
+    {
+        while (!isDie)
+        {
+            if (state == State.DIE) yield break;
+
+            float dist = Vector3.Distance(playerTr.position, enemyTr.position);
+
+            if (dist <= attackDist)
+            {
+                state = State.ATTACK;
+            }
+
+            else if (dist <= traceDist)
+            {
+                state = State.TRACE;
+            }
+            else
+            {
+                state = State.PATROL;
+            }
+
+            yield return ws;
+        }
+    }
+
+    IEnumerator Action()
+    {
+        while (!isDie)
+        {
+            yield return ws;
+
+            switch (state)
+            {
+                case State.PATROL:
+                    moveAgent.patrolling = true;
+                    break;
+                case State.TRACE:
+                    moveAgent.traceTarget = playerTr.position;
+                    break;
+                case State.ATTACK:
+                    moveAgent.Stop();
+                    break;
+                case State.DIE:
+                    moveAgent.Stop();
+                    break;
+            }
+        }
+    }
+}
