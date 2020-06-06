e624c4f890cfc1c29d9725bdf962c620e213853a
hwayuya <hwayuya@gmail.com>
2020-06-07 01:19:08 +0900

    메인화면 버튼 구현 플레이어 HP바 및 총알 상태바 UI구현

--- UnityGuide/Assets/02.Scripts/player/Damage.cs
+++ UnityGuide/Assets/02.Scripts/player/Damage.cs
@@ -1,14 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Damage : MonoBehaviour
 {
 
     private const string bulletTag = "BULLET";
     private const string enemyTag = "ENEMY";
+
     private float initHP = 100.0f;
     public float currHP;
+    public Image bloodScreen;
+
+    public Image hpBar;
+
+    private readonly Color initColor = new Vector4(0, 1.0f, 0.0f, 1.0f);
+    private Color currColor;
 
     public delegate void PlayerDieHandler();
     public static event PlayerDieHandler OnPlayerDie;
@@ -17,7 +26,9 @@ public class Damage : MonoBehaviour
     void Start()
     {
         currHP = initHP;
-        
+
+        hpBar.color = initColor;
+        currColor = initColor;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -26,8 +37,12 @@ public class Damage : MonoBehaviour
         {
             Destroy(other.gameObject);
 
+            StartCoroutine(ShowBloodScreen());
+
             currHP -= 5.0f;
             Debug.Log("Player HP=" + currHP.ToString());
+
+            DisplayHpbar();
 
             if (currHP <= 0.0f)
             {
@@ -36,15 +51,33 @@ public class Damage : MonoBehaviour
         }
     }
 
+    IEnumerator ShowBloodScreen()
+    {
+        bloodScreen.color = new Color(1, 0, 0, UnityEngine.Random.Range(0.2f,0.3f));
+        yield return new WaitForSeconds(0.1f);
+        bloodScreen.color = Color.clear;
+    }
+
     void playerDie()
     {
         OnPlayerDie();
 
-        //Debug.Log("PlayerDie !");
-        //GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
-        //
-        //for(int i = 0; i < enemies.Length; i++) {
-        //    enemies[i].SendMessage("OnPlayerDie", SendMessageOptions.DontRequireReceiver);
-        //}
+        Debug.Log("PlayerDie !");
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        
+        for(int i = 0; i < enemies.Length; i++) {
+            enemies[i].SendMessage("OnPlayerDie", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    private void DisplayHpbar()
+    {
+        if ((currHP / initHP) > 0.5f)
+            currColor.r = (1 - (currHP / initHP)) * 2.0f;
+        else
+            currColor.g = (currHP / initHP) * 2.0f;
+    
+        hpBar.color = currColor;
+        hpBar.fillAmount = (currHP / initHP);
     }
 }
